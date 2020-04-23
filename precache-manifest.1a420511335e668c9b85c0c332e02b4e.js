2d10aebce17bc0376c8500c6c1c0e05409b316f6
Rishu Sahu <rishusahu23@gmail.con>
2020-04-24 03:41:06 +0530

    Updates

---
+++ precache-manifest.1a420511335e668c9b85c0c332e02b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aac903c6fb358590ae73c71487dd767",
+    "url": "/searchbox/index.html"
+  },
+  {
+    "revision": "8414a805da53b89d145f",
+    "url": "/searchbox/static/css/main.f383c381.chunk.css"
+  },
+  {
+    "revision": "123654253f719f0ffc46",
+    "url": "/searchbox/static/js/2.a2c753af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchbox/static/js/2.a2c753af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8414a805da53b89d145f",
+    "url": "/searchbox/static/js/main.966af28f.chunk.js"
+  },
+  {
+    "revision": "e61e058892a620acd9b9",
+    "url": "/searchbox/static/js/runtime-main.70a90878.js"
+  }
+]);
